744bc07f0fffe67e0ee344e8219dc118ecdcb61d
bella <bella5yan@126.com>
2018-11-06 16:19:59 +0800

    add printf&test files

---
+++ printf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Filename:printf.sh
+
+printf "%-5s %-10s %-4s\n" No Name Mark
+
+printf "%-5s %-10s %-4.2f\n" 1 Starath 80.3456
+
+
+
+echo "=================="
+
+echo -e "No\tName\tMark"
+echo -e "1\tStarath\t80.3456"
---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Filename:test.sh
+#######echo "cannot include exclamation - ! within double quotes"
+
+echo -e "1\t2\t3\t4\t5"
+
+echo -e "\e[1;42m Green Background \e[0m"
+
+echo -e "\e[1;31m This is text \e[0m"
